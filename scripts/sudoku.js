66d4a51c62e72b4e0fa4fb17f341462fe329a319
Julien00859 <Julien.castiaux@gmail.com>
2017-11-22 00:35:42 +0100

    Grid, Sudoku & Visualisation

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -1,35 +1,73 @@
-let sudokuGrid;
+let grid;
 
-function getCellAt(block, cell) {
-    return sudokuGrid[block * 9 + cell];
+function getCellAt(blockNo, cellNo) {
+    return grid[blockNo * 9 + cellNo];
 }
 
-function setValue(cell, value) {
+function setCellValue(cell, value) {
     cell.dataset.value = value;
-    if (cell.firstChild)
-        cell.replaceChild(firstChild, SVGNumbers[value-1]);
-    else
-        cell.appendChild(SVGNumbers[value-1]);
+    cell.firstChild.firstChild.nodeValue = value;
 }
 
 function main() {
-    sudokuGrid = document.getElementsByClassName("sudoku-cell");
-    console.log(sudokuGrid)
-    generateGrid()
+    grid = document.getElementsByClassName("sudoku-cell");
+    console.log(grid);
+    generateGrid(document.getElementById("sudoku-grid"));
+    visualize(generateSudoku());
+    console.log(grid);
 }
 
-function shuffle(array) {
-    for (let idx in array) {
-        let rnd = Math.floor(Math.random() * array.length);
-        [array[idx], array[rnd]] = [array[rnd], array[idx]];
-    }
-    return array;
+function visualize(sudoku) {
+    for (row in sudoku)
+        for (col in sudoku[row])
+            setCellValue(getCellAt(Math.floor(row / 3) * 3 + Math.floor(col / 3), (row % 3) * 3 + (col % 3)), sudoku[row][col] || "");
 }
 
-function generateGrid() {
-    const seed = shuffle(new Array(9).fill(0).map((_, idx) => idx + 1));
-    for (let i = 0; i < 9; i++) {
-        setValue(getCellAt(0, i), seed[i]);
+function generateGrid(container) {
+    while (container.firstChild)
+        container.removeNode(container.firstChild);
+    for (let row = 0; row < 9; row++) {
+        const block = document.createElement("div");
+        block.classList.add("sudoku-block");
+        for (let cell = 0; cell < 9; cell++) {
+            const value = document.createElement("p");
+            value.appendChild(document.createTextNode(""))
+            const cell = document.createElement("cell");
+            cell.appendChild(value);
+            cell.classList.add("sudoku-cell");
+            block.appendChild(cell);
+        }
+        console.log(grid);
+        container.appendChild(block);
     }
-        
+}
+
+function generateSudoku() {
+    const sudoku = new Array(9).fill(null).map(function() {return new Array(9).fill(null)});
+    const permutations = new Array(8).fill(null).map(function() {return new Array(9).fill(null)});
+
+    sudoku[0] = _.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    for (let row = 1; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (permutations[row-1][col] === null) {
+                const line = sudoku[row].slice(0, col);
+                const collumn = _.zip.apply(null, sudoku)[col].slice(0, row);
+                const block = _.flatten(sudoku.slice(row - (row % 3), row).map(function(line) {return line.slice(col - (col % 3), col - (col % 3) + 3)}));
+                permutations[row-1][col] = _.chain([1, 2, 3, 4, 5, 6, 7, 8, 9]).difference(_.union(line, collumn, block)).shuffle().value();
+            }
+            if (permutations[row-1][col].length) {
+                sudoku[row][col] = permutations[row-1][col].pop();
+            } else {
+                sudoku[row][col] = null;
+                permutations[row-1][col] = null;
+                if (col == 0) {
+                    row -= 2;
+                    break;
+                } else {
+                    col -= 2;
+                }
+            }
+        }
+    }
+    return sudoku;
 }
